7abc0d7e20945ada686f2183d3e1678a2ac0b04a
masud2005 <masud20012005@gmail.com>
2024-10-25 09:53:04 +0600

    Main Section Added

--- src/components/MainSection/Player.jsx
+++ src/components/MainSection/Player.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
 
-const Player = () => {
+const Player = ({ player }) => {
+    console.log(player)
+    const { image, name, country, role, battingType, bowlingType, biddingPrice } = player;
     return (
-        <div>
-            
-        </div>
+        // <div className="px-2">
+            <div className="card card-compact bg-base-100 shadow-xl">
+                <figure>
+                    <img className="w-full h-[240px] lg:h-[260px] 2xl:h-[275px]" src={image} alt="Player..." />
+                </figure>
+                <div className="card-body">
+                    <div className="flex gap-3">
+                        <img src="https://img.icons8.com/?size=50&id=7819&format=png" alt="Profile" />
+                        <h2 className="card-title">{name}</h2>
+                    </div>
+                    <div className="flex justify-between items-center mb-1">
+                        <div className="flex items-center gap-2">
+                            <img src="https://img.icons8.com/?size=48&id=TyDZNgOeTyqW&format=png" alt="Bangladesh..." />
+                            <p>{country}</p>
+                        </div>
+                        <button className="btn min-h-10 h-0">{role}</button>
+                    </div>
+                    <hr />
+                    <p className="mt-4">Rating</p>
+                    <div className="flex justify-between items-center">
+                        <p>{battingType}</p>
+                        <div>
+                            <p className="text-gray-500">{bowlingType}</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <p>Price: ${biddingPrice}</p>
+                        <button className="btn min-h-10 h-0">Choose Player</button>
+                    </div>
+                </div>
+            </div>
+        // </div>
     );
 };
 
--- src/components/MainSection/Players.jsx
+++ src/components/MainSection/Players.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
+import Player from "./Player";
 
 const Players = () => {
+
+    const [players, setPlayers] = useState([])
+
+    useEffect(() => {
+        fetch('playersInfo.json')
+            .then(res => res.json())
+            .then(data => setPlayers(data));
+    }, [])
+    // console.log(players)
+
     return (
-        <div>
-            
+        <div className="container mx-auto px-2">
+            <div className="flex justify-between items-center mt-20 mb-10">
+                <h1>Available Players</h1>
+                <div className="border border-gray-300 rounded-lg gap-0">
+                    <button className="bg-lime-300 px-3 py-3 rounded-l-lg">Available</button>
+                    <button className="px-3 py-3 rounded-r-lg">Selected(0)</button>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-8">
+                {
+                    players.map(player => <Player player={player} key={player.playerId}></Player>)
+                }
+            </div>
         </div>
     );
 };
